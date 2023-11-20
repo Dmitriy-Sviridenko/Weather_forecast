5e93c127240a0bdfea6f3a995bfaa5898b8792a2
Dmitriy Sviridenko <svi.dim305@gmail.com>
2023-11-20 12:23:54 +0300

    V.0.2

--- readme.txt
+++ readme.txt
@@ -3,3 +3,5 @@ Weather application
 This app use openweathermap API
 
 v.0.1 - current forecast with parametrs like temp, wind speed, hummidity. Eng only
+
+v.0.2 - add forecast weather for 4-days
--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-const apiKey = "5636b1bfe251634f876d672ff385cd52"
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=`
-const searchInput = document.querySelector(".search-box input");
+//Переменные для работы с API
+const ApiKey = "5636b1bfe251634f876d672ff385cd52"
+const currentApiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=`
+const forecastApiUrl = `http://api.openweathermap.org/data/2.5/forecast?units=metric&q=`
+
+//Переменные для кнопок
 const searchButton = document.querySelector(".search-box button");
+const currentWeatherButton = document.querySelector(".form_radio_group--curent");
+const forecastWeatherButton = document.querySelector(".form_radio_group--forecast");
+
+//Остальные переменные
+const searchInput = document.querySelector(".search-box input");
 const weather = document.querySelector(".weather");
-const weatherImage = document.querySelector(".weather__img");
+const weatherImage = document.querySelector(".current-weather__img");
 const error = document.querySelector(".error");
 const errorEmpty = document.querySelector(".error_empty");
-const container = document.querySelector(".container")
+const container = document.querySelector(".container");
+
+
+//массив с картинками
 let imgs = {
   Clear : 'img/clear.svg',
   Rain : 'img/rain.svg',
@@ -14,15 +25,16 @@ let imgs = {
   Drizzle : "img/drizzle.svg",
   Snow : "img/snow.svg",
   Thunderstorm : "img/thunderstorm.svg"
-}
+};
 
 
-async function checkWeather () {
-  //отчищаем поля ошибок
+//Показ текущей погоды
+async function checkCurrentWeather () {
+  // отчищаем поля ошибок
   error.style.display = "none"
   errorEmpty.style.display = "none"
 
-  //Вывод ошибки при отправке пустой формы
+  // Вывод ошибки при отправке пустой формы
   if (searchInput.value === "") {
     errorEmpty.style.display = "block"
     weather.classList.remove("active")
@@ -32,7 +44,7 @@ async function checkWeather () {
   }
 
   //получение данных от API
-  const response = await fetch (apiUrl + searchInput.value + `&appid=${apiKey}`);
+  const response = await fetch (currentApiUrl + searchInput.value + `&appid=${ApiKey}`);
   
   //отработка неправильного название
   if (response.status == 404) {
@@ -46,10 +58,10 @@ async function checkWeather () {
 
   //заполняем данными поля
   document.querySelector(".city").innerHTML = data.name;
-  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "&#8451";
+  document.querySelector(".current_temp").innerHTML = Math.round(data.main.temp) + "&#8451";
   document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
   document.querySelector(".wind").innerHTML = Math.round(data.wind.speed) + " km/h";
-  document.querySelector(".weather_disc").innerHTML = data.weather[0].main
+  document.querySelector(".current_weather_disc").innerHTML = data.weather[0].main
   
 
   //смена иконки погоды
@@ -59,24 +71,107 @@ async function checkWeather () {
     case "Clouds" : weatherImage.src = imgs.Clouds; break;
     case "Drizzle" : weatherImage.src = imgs.Drizzle; break;
     case "Thunderstorm" : weatherImage.src = imgs.Thunderstorm; break;
+    case "Snow" : forecastWeekImg[i].src = imgs.Snow; break;
   }
 
   container.style.height = "570px"
   weather.classList.add("active");
 }
 
+
+//Четырехдневный прогноз
+//переменные для даты
+
+
+async function checkForecastWeather () {
+  error.style.display = "none"
+  errorEmpty.style.display = "none"
+
+  // Вывод ошибки при отправке пустой формы
+  if (searchInput.value === "") {
+    errorEmpty.style.display = "block"
+    weather.classList.remove("active")
+    container.classList.remove("active")
+    container.style.height = "160px"
+    return
+  }
+
+  const response = await fetch (forecastApiUrl + searchInput.value + `&appid=${ApiKey}`);
+
+  //отработка неправильного название
+  if (response.status == 404) {
+    error.style.display = "block"
+    weather.classList.remove("active")
+    container.classList.remove("active")
+    container.style.height = "160px"
+  }
+
+  const data = await response.json();
+
+  console.log(data)
+
+  //заполняем данными поля
+  document.querySelector(".forecast_weather_city").innerHTML = data.city.name;
+  const daysOfWeek = document.querySelectorAll(".forecast_weather_day");
+  const forecastTime = document.querySelectorAll(".forecast_weather_time");
+  const forecastWeekTemp = document.querySelectorAll(".forecast_weather_temp");
+  const forecastWeekDisk = document.querySelectorAll(".forecast_weather_disc"); 
+  const forecastWeekImg =  document.querySelectorAll(".forecast_weather__img");   
+
+  for (let i = 0; i < 4; i++){
+    const curentDate = new Date;
+    curentDate.setDate(curentDate.getDate() + (i+1));
+    daysOfWeek[i].innerHTML = curentDate.getDate() + "-" + (curentDate.getMonth()+1)
+    forecastTime[i].innerHTML = data.list[((i+1)*8)].dt_txt.substr(11, 5);
+
+    forecastWeekTemp[i].innerHTML = Math.round(data.list[((i+1)*8)].main.temp) + "&#8451";
+    forecastWeekDisk[i].innerHTML = data.list[((i+1)*8)].weather[0].main;
+    switch (data.list[((i+1)*8)].weather[0].main) {
+      case "Clear" : forecastWeekImg[i].src = imgs.Clear; break;
+      case "Rain" : forecastWeekImg[i].src = imgs.Rain; break;
+      case "Clouds" : forecastWeekImg[i].src = imgs.Clouds; break;
+      case "Drizzle" : forecastWeekImg[i].src = imgs.Drizzle; break;
+      case "Thunderstorm" : forecastWeekImg[i].src = imgs.Thunderstorm; break;
+      case "Snow" : forecastWeekImg[i].src = imgs.Snow; break;
+    }
+  }
+}
+
+
 //Вызов функции по клику
 searchButton.addEventListener("click", () => {
-  checkWeather()
+  checkCurrentWeather()
+  checkForecastWeather ()
   searchInput.value = ""
 });
 
 //Вызов функции через клавишу Enter
 searchInput.addEventListener("keydown", (e) => {
   if(e.keyCode === 13) {
-    checkWeather()
+    checkCurrentWeather()
+    checkForecastWeather ()
     searchInput.value = ""
   }
 });
+
+currentWeatherButton.addEventListener("click", () => {
+  currentWeatherButton.classList.add("form_radio_group_active")
+  forecastWeatherButton.classList.remove("form_radio_group_active")
+
+  document.querySelector(".current_weather").style.display = "block";
+  document.querySelector(".forecast_weather").style.display = "none"
+});
+
+forecastWeatherButton.addEventListener("click", () => {
+  forecastWeatherButton.classList.add("form_radio_group_active")
+  currentWeatherButton.classList.remove("form_radio_group_active")
+
+
+  document.querySelector(".forecast_weather").style.display = "block"
+  document.querySelector(".current_weather").style.display = "none";
+});
+
+
+
 
 
